2bbb488163ab262a5686767b07f27bf32e632379
RomanK <roman.kvasnikov@gmail.com>
2025-10-01 00:04:09 +0300

    2025-10-01 00:04:09

--- nixos/modules/samba.nix
+++ nixos/modules/samba.nix
@@ -2,29 +2,6 @@
   services = {
     samba = {
       enable = true;
-
-      enableNmbd = true;
-
-      extraConfig = ''
-        workgroup = WORKGROUP
-        server string = Samba Server
-        server role = standalone server
-        log file = /var/log/samba/smbd.%m
-        max log size = 50
-        dns proxy = no
-        map to guest = Bad User
-      '';
-
-      shares = {
-        public = {
-          path = "/home/public";
-          browseable = "yes";
-          "writable" = "yes";
-          "guest ok" = "yes";
-          "public" = "yes";
-          "force user" = "share";
-        };
-      };
     };
   };
 
