d30ca2f0dc149fbf27e42e45f5aa876df7ef958b
RomanK <roman.kvasnikov@gmail.com>
2025-10-01 10:47:44 +0300

    2025-10-01 10:47:44

--- nixos/modules/immich.nix
+++ nixos/modules/immich.nix
@@ -6,6 +6,15 @@
     port = 2283;
 
     openFirewall = true;
+
+    # Настройки Redis для совместимости
+    redis = {
+      enable = true;
+
+      # Принудительно использовать совместимую версию RDB
+      rdbcompression = true;
+      rdbchecksum = true;
+    };
   };
 
   # networking.firewall.allowedTCPPorts = [2283];
