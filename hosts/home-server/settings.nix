f100e6e163a76e6b6623a8dc67371585d6e31829
RomanK <roman.kvasnikov@gmail.com>
2025-10-04 15:38:18 +0300

    2025-10-04 15:38:18

--- hosts/home-server/settings.nix
+++ hosts/home-server/settings.nix
@@ -1,8 +1,27 @@
-{
-  server = {
-    email = "roman.kvasok@gmail.com";
-    domain = "kvasok.xyz";
-    ip = "192.168.1.11";
-    subnet = "192.168.1.0/24";
+{lib, ...}: {
+  options.server = {
+    email = lib.mkOption {
+      type = lib.types.str;
+      description = "Email for ACME registration";
+      default = "roman.kvasok@gmail.com";
+    };
+
+    domain = lib.mkOption {
+      type = lib.types.str;
+      description = "Domain name for the server";
+      default = "kvasok.xyz";
+    };
+
+    ip = lib.mkOption {
+      type = lib.types.str;
+      description = "IP address for the server";
+      default = "192.168.1.11";
+    };
+
+    subnet = lib.mkOption {
+      type = lib.types.str;
+      description = "Subnet for the server";
+      default = "192.168.1.0/24";
+    };
   };
 }
