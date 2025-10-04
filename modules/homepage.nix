633a3b818e85ca88563d021dd703aaf9ddb1529c
RomanK <roman.kvasnikov@gmail.com>
2025-10-04 21:02:11 +0300

    2025-10-04 21:02:11

--- modules/homepage.nix
+++ modules/homepage.nix
@@ -13,6 +13,10 @@
 
     allowedHosts = "cockpit.${config.server.domain}";
 
+    settings = {
+      title = "Kvasnikov's Home Server";
+    };
+
     widgets = [
       {
         resources = {
@@ -33,6 +37,16 @@
               href = "https://immich.${config.server.domain}/";
               siteMonitor = "https://immich.${config.server.domain}/";
             };
+            "Jellyfin" = {
+              description = "Jellyfin";
+              href = "https://jellyfin.${config.server.domain}/";
+              siteMonitor = "https://jellyfin.${config.server.domain}/";
+            };
+            "Nextcloud" = {
+              description = "Nextcloud";
+              href = "https://nextcloud.${config.server.domain}/";
+              siteMonitor = "https://nextcloud.${config.server.domain}/";
+            };
           }
         ];
       }
@@ -41,17 +55,12 @@
           {
             "My Second Service" = {
               description = "Homepage is the best";
-              href = "http://${config.server.domain}/";
+              href = "https://${config.server.domain}/";
             };
           }
         ];
       }
     ];
-
-    settings = {
-      # startUrl: https://custom.url
-      title = "hello world";
-    };
 
     # listenPort=
     # oopenFirewall
