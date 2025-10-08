2da4c46be0ae3ecd5963dbc88d8b5d6a1e47583f
RomanK <roman.kvasnikov@gmail.com>
2025-10-08 08:58:24 +0300

    2025-10-08 08:58:24

--- modules/immich.nix
+++ modules/immich.nix
@@ -36,7 +36,7 @@ in {
         group = cfgServer.systemGroup;
       };
 
-      systemd.tmpfiles.rules = ["d ${cfg.mediaDir} 0775 immich ${cfgServer.systemGroup} - -"];
+      # systemd.tmpfiles.rules = ["d ${cfg.mediaDir} 0775 immich ${cfgServer.systemGroup} - -"];
 
       services.immich = {
         enable = true;
@@ -46,7 +46,7 @@ in {
         user = "immich";
         group = cfgServer.systemGroup;
 
-        mediaLocation = cfg.mediaDir;
+        # mediaLocation = cfg.mediaDir;
       };
     })
 
