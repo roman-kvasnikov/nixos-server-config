2c0d660bb651cc8c978be2997ba34bd324956d3b
RomanK <roman.kvasnikov@gmail.com>
2025-10-09 22:06:23 +0300

    2025-10-09 22:06:23

--- modules/homelab/filebrowser.nix
+++ modules/homelab/filebrowser.nix
@@ -33,7 +33,7 @@ in {
       };
       category = lib.mkOption {
         type = lib.types.str;
-        default = "Downloads";
+        default = "Services";
       };
       widget = lib.mkOption {
         type = lib.types.attrs;
