0618b14bcd24041e275bff0e3dba35bb365c4325
RomanK <roman.kvasnikov@gmail.com>
2025-10-10 22:59:13 +0300

    2025-10-10 22:59:13

--- modules/homelab/nextcloud.nix
+++ modules/homelab/nextcloud.nix
@@ -57,7 +57,7 @@ in {
       services.nextcloud = {
         enable = true;
 
-        package = pkgs.nextcloud31;
+        package = pkgs.nextcloud32;
 
         hostName = cfg.host;
         https = true;
