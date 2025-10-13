2d0d0a2624cb6329db2e2a5232c1271869c76043
RomanK <roman.kvasnikov@gmail.com>
2025-10-13 13:50:35 +0300

    2025-10-13 13:50:35

--- hosts/homelab/services.nix
+++ hosts/homelab/services.nix
@@ -9,72 +9,74 @@
     ../../modules/services
   ];
 
-  services = {
-    acmectl.enable = true;
-    nginxctl.enable = true;
-    hddfancontrolctl.enable = true;
-
-    # delugectl.enable = false;
-    # immichctl.enable = true;
-
-    # sambactl = {
-    #   enable = true;
-
-    #   users = ["romank"];
-
-    #   shares = {
-    #     public = {
-    #       "path" = "/home/public";
-    #     };
-
-    #     movies = {
-    #       "path" = "/home/movies";
-    #     };
-
-    #     romank = {
-    #       "path" = "/home/romank";
-    #       "public" = "no";
-    #       "guest ok" = "no";
-    #       "create mask" = "0770";
-    #       "directory mask" = "0770";
-    #       "force user" = "romank";
-    #       "force group" = "romank";
-    #       "valid users" = ["romank"];
-    #     };
-    #   };
-    # };
-
-    # unifictl.enable = false;
-    # uptime-kumactl.enable = true;
-    # xrayctl.enable = true;
-
-    # Additional services
-    # dbus.enable = true; # Для работы с systemd
-    # udisks2.enable = true; # Автоматическое монтирование USB
-    # geoclue2.enable = true; # Геолокация для часовых поясов
-  };
-
-  homelab = {
-    systemd.tmpfiles.rules = [
-      "d /media 0755 root root - -"
-      "d /media/.torrents 0770 ${config.server.systemUser} ${config.server.systemGroup} - -"
-      "d /media/Downloads 0770 ${config.server.systemUser} ${config.server.systemGroup} - -"
-      "d /media/Movies 0770 ${config.server.systemUser} ${config.server.systemGroup} - -"
-      "d /media/TV\ Shows 0770 ${config.server.systemUser} ${config.server.systemGroup} - -"
-      "d /media/Cartoons 0770 ${config.server.systemUser} ${config.server.systemGroup} - -"
-    ];
-
+  config = {
     services = {
-      cockpitctl.enable = true;
-      filebrowserctl.enable = true;
-      homepagectl.enable = true;
-      immichctl.enable = true;
-      jellyfinctl.enable = true;
-      nextcloudctl.enable = true;
-      qbittorrentctl.enable = true;
-      tdarrctl.enable = false;
-      uptime-kumactl.enable = true;
-      vaultwardenctl.enable = true;
+      acmectl.enable = true;
+      nginxctl.enable = true;
+      hddfancontrolctl.enable = true;
+
+      # delugectl.enable = false;
+      # immichctl.enable = true;
+
+      # sambactl = {
+      #   enable = true;
+
+      #   users = ["romank"];
+
+      #   shares = {
+      #     public = {
+      #       "path" = "/home/public";
+      #     };
+
+      #     movies = {
+      #       "path" = "/home/movies";
+      #     };
+
+      #     romank = {
+      #       "path" = "/home/romank";
+      #       "public" = "no";
+      #       "guest ok" = "no";
+      #       "create mask" = "0770";
+      #       "directory mask" = "0770";
+      #       "force user" = "romank";
+      #       "force group" = "romank";
+      #       "valid users" = ["romank"];
+      #     };
+      #   };
+      # };
+
+      # unifictl.enable = false;
+      # uptime-kumactl.enable = true;
+      # xrayctl.enable = true;
+
+      # Additional services
+      # dbus.enable = true; # Для работы с systemd
+      # udisks2.enable = true; # Автоматическое монтирование USB
+      # geoclue2.enable = true; # Геолокация для часовых поясов
+    };
+
+    homelab = {
+      systemd.tmpfiles.rules = [
+        "d /media 0755 root root - -"
+        "d /media/.torrents 0770 ${config.server.systemUser} ${config.server.systemGroup} - -"
+        "d /media/Downloads 0770 ${config.server.systemUser} ${config.server.systemGroup} - -"
+        "d /media/Movies 0770 ${config.server.systemUser} ${config.server.systemGroup} - -"
+        "d /media/TV\ Shows 0770 ${config.server.systemUser} ${config.server.systemGroup} - -"
+        "d /media/Cartoons 0770 ${config.server.systemUser} ${config.server.systemGroup} - -"
+      ];
+
+      services = {
+        cockpitctl.enable = true;
+        filebrowserctl.enable = true;
+        homepagectl.enable = true;
+        immichctl.enable = true;
+        jellyfinctl.enable = true;
+        nextcloudctl.enable = true;
+        qbittorrentctl.enable = true;
+        tdarrctl.enable = false;
+        uptime-kumactl.enable = true;
+        vaultwardenctl.enable = true;
+      };
     };
   };
 }
