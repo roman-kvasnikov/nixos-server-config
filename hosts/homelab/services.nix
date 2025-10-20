11b1e7d1102d08c6fc8b1a350a790b4471c2a133
RomanK <roman.kvasnikov@gmail.com>
2025-10-20 18:03:29 +0300

    2025-10-20 18:03:29

--- hosts/homelab/services.nix
+++ hosts/homelab/services.nix
@@ -24,18 +24,18 @@
       # geoclue2.enable = true; # Геолокация для часовых поясов
     };
 
-    age.secrets = {
-      romank-samba-password = {
-        file = ./secrets/samba/romank-password.age;
-        owner = "root";
-        mode = "0600";
-      };
-      dssmargo-samba-password = {
-        file = ./secrets/samba/dssmargo-password.age;
-        owner = "root";
-        mode = "0600";
-      };
-    };
+    # age.secrets = {
+    #   romank-samba-password = {
+    #     file = ./secrets/samba/romank-password.age;
+    #     owner = "root";
+    #     mode = "0600";
+    #   };
+    #   dssmargo-samba-password = {
+    #     file = ./secrets/samba/dssmargo-password.age;
+    #     owner = "root";
+    #     mode = "0600";
+    #   };
+    # };
 
     homelab = {
       services = {
@@ -47,50 +47,50 @@
         nextcloudctl.enable = true;
         qbittorrentctl.enable = true;
 
-        sambactl = {
-          enable = true;
+        # sambactl = {
+        #   enable = true;
 
-          users = {
-            romank = {
-              passwordFile = config.age.secrets.romank-samba-password.path;
-            };
-            dssmargo = {
-              passwordFile = config.age.secrets.dssmargo-samba-password.path;
-            };
-          };
+        #   users = {
+        #     romank = {
+        #       passwordFile = config.age.secrets.romank-samba-password.path;
+        #     };
+        #     dssmargo = {
+        #       passwordFile = config.age.secrets.dssmargo-samba-password.path;
+        #     };
+        #   };
 
-          shares = {
-            Public = {
-              path = "/mnt/Shares/Public";
-              comment = "Public Share for Everyone";
-              public = true;
-              browseable = true;
-              writeable = true;
-            };
+        #   shares = {
+        #     Public = {
+        #       path = "/mnt/Shares/Public";
+        #       comment = "Public Share for Everyone";
+        #       public = true;
+        #       browseable = true;
+        #       writeable = true;
+        #     };
 
-            RomanK = {
-              path = "/mnt/Shares/RomanK";
-              comment = "RomanK's Private Share";
-              public = false;
-              browseable = true;
-              writeable = true;
-              validUsers = ["romank"];
-              forceUser = "romank";
-              forceGroup = "users";
-            };
+        #     RomanK = {
+        #       path = "/mnt/Shares/RomanK";
+        #       comment = "RomanK's Private Share";
+        #       public = false;
+        #       browseable = true;
+        #       writeable = true;
+        #       validUsers = ["romank"];
+        #       forceUser = "romank";
+        #       forceGroup = "users";
+        #     };
 
-            DssMargo = {
-              path = "/mnt/Shares/DssMargo";
-              comment = "DssMargo's Private Share";
-              public = false;
-              browseable = true;
-              writeable = true;
-              validUsers = ["dssmargo"];
-              forceUser = "dssmargo";
-              forceGroup = "users";
-            };
-          };
-        };
+        #     DssMargo = {
+        #       path = "/mnt/Shares/DssMargo";
+        #       comment = "DssMargo's Private Share";
+        #       public = false;
+        #       browseable = true;
+        #       writeable = true;
+        #       validUsers = ["dssmargo"];
+        #       forceUser = "dssmargo";
+        #       forceGroup = "users";
+        #     };
+        #   };
+        # };
 
         # tdarrctl.enable = true;
         # unifictl.enable = false;
