9f3f0e0c3e2604eabb3c874da15145e000936691
RomanK <roman.kvasnikov@gmail.com>
2025-10-20 18:59:24 +0300

    2025-10-20 18:59:24

--- secrets.nix
+++ secrets.nix
@@ -1,6 +1,8 @@
 let
   homelabSshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP+APlAyYDiSgtzG2W8KmwWEWW33MdwXMUDxdTThW9Jm root@homelab";
 in {
+  "./secrets/nextcloud/admin-password.age".publicKeys = [homelabSshKey];
+
   "./secrets/samba/romank-password.age".publicKeys = [homelabSshKey];
   "./secrets/samba/dssmargo-password.age".publicKeys = [homelabSshKey];
 }
