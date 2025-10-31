c27099674ae359dd6fb1eff9d0b459bdca310a8d
RomanK <roman.kvasnikov@gmail.com>
2025-10-31 13:50:12 +0300

    2025-10-31 13:50:12

--- modules/services/postgresql.nix
+++ modules/services/postgresql.nix
@@ -3,9 +3,13 @@
   pkgs,
   ...
 }: {
+  systemd.tmpfiles.rules = [
+    "d /data/postgresql/${config.services.postgresql.package.psqlSchema} 0755 postgres postgres - -"
+  ];
+
   services.postgresql = {
     enable = true;
 
-    dataDir = "/data/postgresql";
+    dataDir = "/data/postgresql/${config.services.postgresql.package.psqlSchema}";
   };
 }
