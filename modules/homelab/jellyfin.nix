7c8032ebb61dc6f15cf634d1a62f681ae398a9a2
RomanK <roman.kvasnikov@gmail.com>
2025-11-01 12:19:55 +0300

    2025-11-01 12:19:55

--- modules/homelab/jellyfin.nix
+++ modules/homelab/jellyfin.nix
@@ -151,15 +151,21 @@ in {
             extraConfig = ''
               client_max_body_size 20M;
 
+              # Security / XSS Mitigation Headers
               add_header X-Content-Type-Options "nosniff";
-              add_header Referrer-Policy "no-referrer-when-downgrade";
-              add_header X-Frame-Options "SAMEORIGIN";
-              add_header X-XSS-Protection "1; mode=block";
+
+              # Permissions policy. May cause issues with some clients
+              add_header Permissions-Policy "accelerometer=(), ambient-light-sensor=(), battery=(), bluetooth=(), camera=(), clipboard-read=(), display-capture=(), document-domain=(), encrypted-media=(), gamepad=(), geolocation=(), gyroscope=(), hid=(), idle-detection=(), interest-cohort=(), keyboard-map=(), local-fonts=(), magnetometer=(), microphone=(), payment=(), publickey-credentials-get=(), serial=(), sync-xhr=(), usb=(), xr-spatial-tracking=()" always;
+
+              # Content Security Policy
+              # See: https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+              # Enforces https content and restricts JS/CSS to origin
+              # External Javascript (such as cast_sender.js for Chromecast) must be whitelisted.
+              add_header Content-Security-Policy "default-src https: data: blob: ; img-src 'self' https://* ; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' https://www.gstatic.com https://www.youtube.com blob:; worker-src 'self' blob:; connect-src 'self'; object-src 'none'; font-src 'self'";
             '';
 
             locations."/" = {
               proxyPass = "http://127.0.0.1:8096";
-              proxyWebsockets = true;
               recommendedProxySettings = true;
 
               extraConfig = ''
@@ -167,6 +173,17 @@ in {
                 proxy_set_header X-Forwarded-Host     $http_host;
 
                 proxy_buffering off;
+              '';
+            };
+
+            locations."/socket" = {
+              proxyPass = "http://127.0.0.1:8096";
+              proxyWebsockets = true;
+              recommendedProxySettings = true;
+
+              extraConfig = ''
+                proxy_set_header X-Forwarded-Protocol $scheme;
+                proxy_set_header X-Forwarded-Host     $http_host;
               '';
             };
           };
