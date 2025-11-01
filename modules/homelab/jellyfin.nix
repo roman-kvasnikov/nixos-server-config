53fb15c4ad4098538570debc257fb54e9bad85c3
RomanK <roman.kvasnikov@gmail.com>
2025-11-01 12:26:05 +0300

    2025-11-01 12:26:05

--- modules/homelab/jellyfin.nix
+++ modules/homelab/jellyfin.nix
@@ -166,6 +166,7 @@ in {
 
             locations."/" = {
               proxyPass = "http://127.0.0.1:8096";
+              proxyWebsockets = true;
               recommendedProxySettings = true;
 
               extraConfig = ''
