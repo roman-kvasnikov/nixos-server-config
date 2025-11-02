3e7c1efd0991ee32baec7ae1f76aa50feb251eba
RomanK <roman.kvasnikov@gmail.com>
2025-11-02 09:59:28 +0300

    2025-11-02 09:59:28

--- hosts/homelab/security.nix
+++ hosts/homelab/security.nix
@@ -25,13 +25,14 @@
       interfaces = {
         wg0 = {
           ips = ["10.0.0.2/24"];
-          listenPort = 51820;
+          # listenPort = 51820;
           privateKey = "qOAgXb4UMZQja0U9mawZmWMDYALiY83q+pxrlnswFVk=";
 
           peers = [
             {
               allowedIPs = ["10.0.0.1/32"];
               publicKey = "tEeF3aLdO7Oka3didAHSFXdDfSVY1PsqpRW/c++sbVI=";
+              endpoint = "77.232.136.6:51820";
               persistentKeepalive = 25;
             }
           ];
