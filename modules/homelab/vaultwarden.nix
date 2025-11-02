9d2092ce2740c5bd31fa4e45c82fb70ed69c0fea
RomanK <roman.kvasnikov@gmail.com>
2025-11-02 21:56:10 +0300

    2025-11-02 21:56:10

--- modules/homelab/vaultwarden.nix
+++ modules/homelab/vaultwarden.nix
@@ -72,21 +72,15 @@ in {
             forceSSL = cfgAcme.enable;
             http2 = true;
 
+            extraConfig = lib.mkIf (!cfg.allowExternal) ''
+              allow ${cfgHomelab.subnet};
+              deny all;
+            '';
+
             locations."/" = {
               proxyPass = "http://127.0.0.1:${toString config.services.vaultwarden.config.ROCKET_PORT}";
               proxyWebsockets = true;
               recommendedProxySettings = true;
-
-              extraConfig = ''
-                ${
-                  if cfg.allowExternal
-                  then ""
-                  else ''
-                    allow ${cfgHomelab.subnet};
-                    deny all;
-                  ''
-                }
-              '';
             };
           };
         };
