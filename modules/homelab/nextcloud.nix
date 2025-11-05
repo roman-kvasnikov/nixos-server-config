f7c7c6dc565e496f54289741c898d663b55df30e
RomanK <roman.kvasnikov@gmail.com>
2025-11-05 16:12:46 +0300

    2025-11-05 16:12:46

--- modules/homelab/nextcloud.nix
+++ modules/homelab/nextcloud.nix
@@ -51,7 +51,7 @@ in {
     apps = lib.mkOption {
       description = "List of Nextcloud apps to enable";
       type = lib.types.listOf lib.types.str;
-      default = ["bookmarks" "calendar" "contacts" "notes" "mail" "onlyoffice" "groupfolders"];
+      default = [];
     };
 
     backupEnabled = lib.mkOption {
@@ -104,10 +104,10 @@ in {
           hostName = cfg.domain;
           https = true;
 
-          extraApps = lib.genAttrs cfg.apps (app: config.services.nextcloud.package.packages.apps.${app});
+          # extraApps = lib.genAttrs cfg.apps (app: config.services.nextcloud.package.packages.apps.${app});
+          appstoreEnable = true;
           extraAppsEnable = true;
           autoUpdateApps.enable = true;
-          appstoreEnable = true;
 
           maxUploadSize = "16G";
 
