7cc00b1dc21590c649cbf736051017e986a5d2ea
RomanK <roman.kvasnikov@gmail.com>
2025-11-12 22:05:09 +0300

    2025-11-12 22:05:09

--- hosts/homelab-backup/services.nix
+++ hosts/homelab-backup/services.nix
@@ -10,24 +10,4 @@ in {
     ../../modules/programs
     ../../modules/services
   ];
-
-  config = {
-    homelab = {
-      services = {
-        sambactl = {
-          enable = true;
-
-          shares = {
-            Shared = {
-              directory = "Shared";
-              comment = "Shared files for everyone";
-              public = true;
-              browseable = true;
-              writeable = true;
-            };
-          };
-        };
-      };
-    };
-  };
 }
