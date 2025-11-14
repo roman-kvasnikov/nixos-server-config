b69b60bee5f3662138a21945ba40749c0a4e83ee
RomanK <roman.kvasnikov@gmail.com>
2025-11-14 22:58:10 +0300

    2025-11-14 22:58:10

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -1,8 +1,24 @@
 {
   config,
+  lib,
   pkgs,
   ...
-}: {
+}: let
+  zfsCompatibleKernelPackages =
+    lib.filterAttrs (
+      name: kernelPackages:
+        (builtins.match "linux_[0-9]+_[0-9]+" name)
+        != null
+        && (builtins.tryEval kernelPackages).success
+        && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
+    )
+    pkgs.linuxKernel.packages;
+  latestKernelPackage = lib.last (
+    lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
+      builtins.attrValues zfsCompatibleKernelPackages
+    )
+  );
+in {
   boot = {
     loader = {
       systemd-boot = {
@@ -16,7 +32,7 @@
     };
 
     # kernelPackages = pkgs.linuxPackages_latest;
-    kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+    kernelPackages = latestKernelPackage;
 
     kernel.sysctl = {
       # Увеличить лимиты для файловых дескрипторов
