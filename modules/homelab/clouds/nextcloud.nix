ed44e430792390021b73ac5e08c4ddd0d2dba6dc
RomanK <roman.kvasnikov@gmail.com>
2025-12-03 00:50:49 +0300

    2025-12-03 00:50:49

--- modules/homelab/clouds/nextcloud.nix
+++ modules/homelab/clouds/nextcloud.nix
@@ -172,7 +172,7 @@ in {
 
         pgbouncer.settings = {
           databases = {
-            "nextcloud" = "host=/run/postgresql dbname=nextcloud auth_user=nextcloud";
+            "nextcloud" = "host=/run/postgresql port=5432 dbname=nextcloud auth_user=nextcloud pool_mode=session";
           };
         };
 
--- modules/services/pgbouncer.nix
+++ modules/services/pgbouncer.nix
@@ -10,10 +10,8 @@ in {
 
     settings = {
       pgbouncer = {
-        listen_addr = "127.0.0.1";
-        listen_port = 6432;
-        pool_mode = "session";
-        auth_type = "trust";
+        listen_addr = "*";
+        auth_type = "peer";
       };
     };
   };
