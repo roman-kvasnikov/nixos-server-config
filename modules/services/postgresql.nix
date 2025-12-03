2f7123ca6055da594e67fc4c317bf5106b109179
RomanK <roman.kvasnikov@gmail.com>
2025-12-03 17:16:42 +0300

    2025-12-03 17:16:42

--- modules/services/postgresql.nix
+++ modules/services/postgresql.nix
@@ -23,29 +23,18 @@ in {
       host all all ::1/128 md5
     '';
 
-    identMap = ''
-      pgbouncer pgbouncer postgres
-      pgbouncer postgres  postgres
-    '';
-
     # identMap = ''
-    #   # Mapping        SystemUser     DatabaseUser
-    #   # -----------------------------------------
-    #   # pgbouncer может представляться любым из этих пользователей
-    #   pgbouncer        pgbouncer       postgres
-    #   pgbouncer        pgbouncer       immich
-    #   pgbouncer        pgbouncer       linkwarden
-    #   pgbouncer        pgbouncer       nextcloud
-    #   pgbouncer        pgbouncer       paperless
-    #   pgbouncer        pgbouncer       vaultwarden
-
-    #   # сервисы напрямую (если будут подключаться без pgbouncer)
-    #   pgbouncer        postgres        postgres
-    #   pgbouncer        immich          immich
-    #   pgbouncer        linkwarden      linkwarden
-    #   pgbouncer        nextcloud       nextcloud
-    #   pgbouncer        paperless       paperless
-    #   pgbouncer        vaultwarden     vaultwarden
+    #   pgbouncer pgbouncer postgres
+    #   pgbouncer postgres  postgres
     # '';
+
+    identMap = ''
+      pgbouncer        pgbouncer       postgres
+
+      pgbouncer        postgres        postgres
+      pgbouncer        immich          immich
+      pgbouncer        linkwarden      linkwarden
+      pgbouncer        paperless       paperless
+    '';
   };
 }
