d23a95cd1c200b2b63e6a6378765da44444164ea
RomanK <roman.kvasnikov@gmail.com>
2025-12-04 15:25:42 +0300

    2025-12-04 15:25:42

--- modules/services/postgresql.nix
+++ modules/services/postgresql.nix
@@ -16,7 +16,7 @@ in {
 
     authentication = ''
       # Локальные сокеты - pgbouncer
-      local all all peer
+      # local all all peer
 
       # Локальные сокеты - pgbouncer
       #local all all peer map=pgbouncer
