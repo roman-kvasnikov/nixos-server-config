c4866c8befa074b31e7be0708dbcc5a9785e98ff
RomanK <roman.kvasnikov@gmail.com>
2025-12-04 11:19:22 +0300

    2025-12-04 11:19:22

--- modules/homelab/clouds/immich.nix
+++ modules/homelab/clouds/immich.nix
@@ -99,9 +99,13 @@ in {
 
           mediaLocation = cfg.dataDir;
 
-          database = {
-            host = "127.0.0.1";
-            port = 6432;
+          # database = {
+          #   host = "127.0.0.1";
+          #   port = 6432;
+          # };
+
+          environment = {
+            DB_URL = lib.mkForce "postgresql:///${config.services.immich.database.name}?host=/run/pgbouncer&port=6432";
           };
         };
 
