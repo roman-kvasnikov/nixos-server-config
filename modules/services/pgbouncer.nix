0735110dc4c4f3ed20f0b2e0c6b80d7951bd831b
RomanK <roman.kvasnikov@gmail.com>
2025-12-04 17:23:30 +0300

    2025-12-04 17:23:30

--- modules/services/pgbouncer.nix
+++ modules/services/pgbouncer.nix
@@ -27,7 +27,7 @@ in {
 
       databases = {
         "*" = "host=/run/postgresql port=5432";
-        # "immich" = "host=/run/postgresql port=5432 dbname=immich";
+        "immich" = "host=/run/postgresql port=5432 dbname=immich";
       };
     };
   };
